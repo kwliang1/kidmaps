c42c54457a809561b69510a77e36349167ca123a
kwliang1 <kevwuliang@gmail.com>
2022-11-26 01:17:40 -0500

    Added additional remote pattern for gmap photo service (post redirect)

--- next.config.js
+++ next.config.js
@@ -8,6 +8,11 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'maps.googleapis.com',
         pathname: '/maps/api/place/js/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/places/**'
       }
     ]
   }
